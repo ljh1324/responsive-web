6820f843bc7db03a6fa81c2d67954e9b5a046713
hwanlee <ljhw3377@gmail.com>
2020-04-22 13:07:56 +0900

    feat: 여행 정보 추가 및 렌더링
    - 여행 정보가 있는 data.js 추가
    - 여행 정보를 불러와 렌더링

--- main.js
+++ main.js
@@ -6,10 +6,29 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
+function getPhotoTemplate(name, image, contents) {
+  return `
+  <div class="col-lg-3 col-sm-6 col-12 mt-5">
+    <div class="container h-100 border rounded">
+      <div class="row">
+        <div class="col-sm-12 col-6 d-flex flex-column justify-content-center">
+          <h3 class="mb-3 text-center pt-3">${name}</h3>
+          <img src="${image}" alt="">
+        </div>
+        <div class="col-sm-12 col-6">
+          <p class="p-2">${contents}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
+}
+
 window.onload = function () {
   const mySidenav = document.getElementById("mySidenav");
   const openBtn = document.getElementById("openBtn");
   const closeBtn = document.getElementById("closeBtn");
+  const photoes = document.getElementById("photoes");
 
   openBtn.addEventListener('click', () => {
     mySidenav.style.width = "250px";
@@ -17,5 +36,11 @@ window.onload = function () {
 
   closeBtn.addEventListener('click', () => {
     mySidenav.style.width = "0";
-  })
+  });
+
+  photoes.innerHTML = data.reduce((acc, item) => {
+    const { name, image, contents } = item;
+    acc += getPhotoTemplate(name, image, contents);
+    return acc;
+  }, '');
 }
